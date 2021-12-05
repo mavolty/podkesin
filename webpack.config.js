2dc31d24eb9a6a17cb9bc13e16763f4499d09032
Saddam Husain <sadamyggakpernahp@gmail.com>
2021-12-05 23:10:50 +0700

    feat: Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  devServer: {
+    static: './dist',
   },
   module: {
     rules: [
